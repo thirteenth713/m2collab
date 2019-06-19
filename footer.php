5ddc29696348911b13999dd23e36521eea3c90fd
Artem <thirteenth713@gmail.com>
2019-06-19 14:56:25 +0300

    add basic html

--- footer.php
+++ footer.php
@@ -11,24 +11,11 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'm2col' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'm2col' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'm2col' ), 'm2col', '<a href="http://underscores.me/">Thirteent</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+</div>
+	
+		</div><!-- bacg-tone-->
+	</div><!-- .main-wrapper-->
+</div><!-- .main-wrapper-mob -->
 
 <?php wp_footer(); ?>
 
